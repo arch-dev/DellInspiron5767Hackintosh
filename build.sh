99498b6ad0b4ebc4347790ebdcbd73e5e27514ae
arch-dev <sebastiano.galeazzo@gmail.com>
2022-04-22 16:16:24 +0200

    Temporary disable ComboJack (not tested yet)

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Created by arch-dev on 02/06/2020
-# Updated by arch-dev on 22/04/2021
+# Updated by arch-dev on 22/04/2022
 # Copyright © 2021 ArchSoftware Inc. All rights reserved.
 
 TOOLS="Tools"
@@ -40,9 +40,11 @@ function download()
   if [[ $(echo $link | cut -d '/' -f 4) == "acidanthera" && $(echo $link | cut -d '/' -f 6) == "releases" ]]; then
    curl -L $link/download/$version/$name-$version-RELEASE.zip > $DIR/$name.zip &
   elif [[ $(echo $link | cut -d '/' -f 5) == "itlwm" && $(echo $link | cut -d '/' -f 6) == "releases" ]]; then
-   curl -L $link/download/$version/$name\_$version\_stable_BigSur.kext.zip > $DIR/$name.zip &
+   curl -L $link/download/$version/$name\_$version\_stable_Monterey.kext.zip > $DIR/$name.zip &
   elif [[ $(echo $link | cut -d '/' -f 5) == "IntelBluetoothFirmware" && $(echo $link | cut -d '/' -f 6) == "releases" ]]; then
-   curl -L $link/download/$version/$name.zip > $DIR/$name.zip &
+   curl -L $link/download/$version/$name-$version.zip > $DIR/$name.zip &
+  elif [[ $(echo $link | cut -d '/' -f 4) == "0xFireWolf" && $(echo $link | cut -d '/' -f 6) == "releases" ]]; then
+   curl -L $link/download/$version/$name\_${version:1:5}\_b998818_RELEASE.zip > $DIR/$name.zip &
   elif [[ $(echo $link | cut -d '/' -f 6) == "releases" ]]; then
    curl -L $link/download/$version/$name-$version.zip > $DIR/$name.zip &
   elif [[ $(echo $link | cut -d '/' -f 6) == "archive" ]]; then
@@ -76,8 +78,18 @@ mkdir $TOOLS
 download $TOOLS "Dependencies/tools.txt"
 echo Copying files to EFI...
 find $TEMP -name \*.kext -exec cp -R {} $OUTDIR/EFI/OC/Kexts \;
+rm -rf $OUTDIR/EFI/OC/Kexts/AppleALCU.kext
+rm -rf $OUTDIR/EFI/OC/Kexts/BrcmBluetoothInjector.kext
+rm -rf $OUTDIR/EFI/OC/Kexts/BrcmBluetoothInjectorLegacy.kext
+rm -rf $OUTDIR/EFI/OC/Kexts/BrcmFirmwareData.kext
+rm -rf $OUTDIR/EFI/OC/Kexts/BrcmFirmwareRepo.kext
+rm -rf $OUTDIR/EFI/OC/Kexts/BrcmNonPatchRAM.kext
+rm -rf $OUTDIR/EFI/OC/Kexts/BrcmNonPatchRAM2.kext
+rm -rf $OUTDIR/EFI/OC/Kexts/BrcmPatchRAM.kext
+rm -rf $OUTDIR/EFI/OC/Kexts/BrcmPatchRAM2.kext
+rm -rf $OUTDIR/EFI/OC/Kexts/BrcmPatchRAM3.kext
+rm -rf $OUTDIR/EFI/OC/Kexts/IntelBluetoothInjector.kext
 rm -rf $OUTDIR/EFI/OC/Kexts/SMCLightSensor.kext
-rm -rf $OUTDIR/EFI/OC/Kexts/SMCDellSensors.kext
 rm -rf $OUTDIR/EFI/OC/Kexts/SMCProcessor.kext
 rm -rf $OUTDIR/EFI/OC/Kexts/SMCSuperIO.kext
 rm -rf $OUTDIR/EFI/OC/Kexts/VoodooInput.kext
@@ -91,8 +103,8 @@ rm -rf $OUTDIR/EFI/OC/Kexts/VoodooPS2Keyboard.kext
 rm -rf $OUTDIR/EFI/OC/Kexts/VoodooPS2Mouse.kext
 rm -rf $OUTDIR/EFI/OC/Kexts/VoodooPS2Trackpad.kext
 cp -R Prebuilt/*.kext $OUTDIR/EFI/OC/Kexts
-cp -R Audio/ComboJack_Installer/VerbStub.kext $OUTDIR/EFI/OC/Kexts
-echo Run Audio/ComboJack_Installer/install.sh script once booted the first time!!
+#cp -R Audio/ComboJack_Installer/VerbStub.kext $OUTDIR/EFI/OC/Kexts
+#echo Run Audio/ComboJack_Installer/install.sh script once booted the first time!!
 while IFS= read -r line; do
  cp $TEMP/$(echo $line | cut -d ',' -f 1) $OUTDIR/$(echo $line | cut -d ',' -f 2)
 done <"Dependencies/efi.txt"
